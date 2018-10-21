5accc963568d26aa4b6e534ff177cdacb1b19134
Patrick Bollinger <owner@pjbollinger.com>
2018-10-21 16:56:37 -0400

    Added lint for JavaScript files.
    Decided to go off of the "standard" JS style guide. Big thing is just staying consistent.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 5000;
+const path = require('path')
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5000
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, '/public')))
 
-app.listen(PORT);
+app.listen(PORT)
