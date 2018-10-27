bc308d5d5039bf58e35381855d8112d20bb6f4b3
Patrick Bollinger <owner@pjbollinger.com>
2018-10-27 14:38:53 -0400

    Added unit tests and fix minor things.
    I added the basic UI unit tests in this commit. Also setup the server to run to perform the UI tests automatically. Also cleaned up some files and added tests to lint check.

--- src/app.js
+++ src/app.js
@@ -5,4 +5,9 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname, '/public')))
 
-app.listen(PORT)
+var server = app.listen(PORT, function () {
+  var port = server.address().port
+  console.log('App is now listening at port %s', port)
+})
+
+module.exports = server
--- test/bootstrap.js
+++ test/bootstrap.js
@@ -1,25 +1,26 @@
-const puppeteer = require('puppeteer');
-const { expect } = require('chai');
-const _ = require('lodash');
-const globalVariables = _.pick(global, ['browser', 'expect']);
+const puppeteer = require('puppeteer')
+const { expect } = require('chai')
+const server = require('../src/app.js')
+const _ = require('lodash')
+const globalVariables = _.pick(global, ['browser', 'expect'])
 
 // puppeteer options
 const opts = {
   headless: true,
-  //slowMo: 100,
+  // slowMo: 100,
   timeout: 10000
-};
+}
 
 // expose variables
-before (async function () {
-  global.expect = expect;
-  global.browser = await puppeteer.launch(opts);
-});
+before(async function () {
+  global.expect = expect
+  global.browser = await puppeteer.launch(opts)
+})
 
 // close browser and reset global variables
-after (function () {
-  browser.close();
-
-  global.browser = globalVariables.browser;
-  global.expect = globalVariables.expect;
-});
+after(function () {
+  browser.close()
+  server.close()
+  global.browser = globalVariables.browser
+  global.expect = globalVariables.expect
+})
